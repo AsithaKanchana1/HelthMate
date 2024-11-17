3a0e16c286e50e20518b0cebd0b75cb663a75490
Asitha Kanchana Palliyaguru <114346362+AsithaKanchana1@users.noreply.github.com>
2024-11-17 11:45:36 +0530

    HelthMateApp @v1.0

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,80 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
+export default function App() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <View style={styles.container}>
+      <Text style={styles.click}>Helth Mate</Text>
+      <Text style={styles.clickText}>Stay Safe, Stay Healthy</Text>
+      {/* Image Container */}
+      <View style={styles.imageContainer}>
+        <Image 
+          source={require('../../assets/images/helthmate_logo.png')}
+          style={{
+            width: 200,
+            height: 200,
+            marginBottom: 10,
+            marginTop: 0,
+          }}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      </View>
+      <TouchableOpacity onPress={() => router.push('/login')} style={styles.logInBtn}>
+        <Text style={styles.btnText}>Let's Go</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    marginTop: 0,
+    backgroundColor: 'white',
+    flex: 1,
+    justifyContent: 'center',  // Centers the content vertically
+    alignItems: 'center',      // Centers the content horizontally
+  },
+  imageContainer: {
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  click: {
+    marginTop: 50,
+    marginBottom: 5,
+    fontWeight: 'bold',
+    fontSize: 35,
+    textAlign: 'center',
+    margin: 10,
+    color: 'white',
+    fontFamily: 'sans-serif', 
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  clickText: {
+    marginTop: 0,
+    marginBottom: 10,
+    fontWeight: 'bold',
+    fontSize: 10,
+    textAlign: 'center',
+    margin: 10,
+    color: 'white',
+    fontFamily: 'sans-serif', 
+  },
+  logInBtn: {
+    marginTop: 50,
+    borderRadius: 30,
+    shadowColor:'yellow',
+    shadowRadius:10,
+    backgroundColor: '#DC7676',
+    padding: 15,
+    width: "50%"
+  },
+  btnText: {
+    color:'white',
+    fontSize: 18,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    flexDirection: 'row',  
+    width: "80%",
   },
 });
